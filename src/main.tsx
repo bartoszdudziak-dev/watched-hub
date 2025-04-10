3aca60c9c49489edef0829b03a44f4fd03aca3b2
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-04-10 14:47:10 +0200

    Baseline fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <BrowserRouter basename="/task-react-routing">
+        <BrowserRouter basename="/watched-hub">
           <App />
         </BrowserRouter>
       </ThemeProvider>
